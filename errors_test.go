3d6e4411d2a3569b019850df4b12fac47c2f3a45
Sergey Keplin <lava.mobi@gmail.com>
2026-08-02 01:42:56 +0300

    Updates

--- errors_test.go
+++ errors_test.go
@@ -7,8 +7,12 @@ import (
 
 func TestBreakpoint(t *testing.T) {
 
-	p := Unavailable("some")
+	var p E //Unavailable("some")
 	p.AddPoint()
+	p.AddPoint("nice")
+	p.Set("ok1", 44131)
+	p.Set("ok2", true)
+	p.Set("ok3", "yeee")
 
 	fmt.Println(p.String())
 }
